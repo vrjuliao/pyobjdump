1d0f3d72562bb3f3e4bcba2724e539e9e9952ee7
V. Julião <viniciusjuliao@dcc.ufmg.br>
2021-11-07 01:17:56 -0300

    parse(instructions): create a dictionary that organizes the instructions sequence of each function

--- objdump.py
+++ objdump.py
@@ -8,7 +8,12 @@ class Objdump:
       self.__mount_symbols_list()
       self.__parse_local_functions()
 
-  def __mount_symbols_list(self): 
+  def __mount_symbols_list(self):
+    dot_text_section = self.__elf.get_section_by_name('.text')
+    local_code_begin = dot_text_section['sh_addr']
+    local_code_end = local_code_begin + dot_text_section.data_size
+    print(f'{local_code_begin:x}, {local_code_end:x}')
+
     # stores all local function names
     self.__local_symbols = dict()
 
@@ -23,25 +28,38 @@ class Objdump:
     #       Unfortunatelly it is a limitation of pyelftools library
     local_symtab = self.__elf.get_section_by_name('.symtab')
     for _, sym in enumerate(local_symtab.iter_symbols()):
-      if(sym.entry['st_info']['type'] == 'STT_FUNC'):
+      addr = sym.entry['st_value']
+      # Ensures that it is getting only functions that have its body inside
+      # '.text' section at the ELF file
+      if(sym.entry['st_info']['type'] == 'STT_FUNC'
+      and addr >= local_code_begin
+      and addr < local_code_end):
         self.__local_symbols[sym.name] = []
-        self.__local_symbols_address.append((sym.entry['st_value'], sym.name))
+        self.__local_symbols_address.append((addr, sym.name))
     
     global_symtab = self.__elf.get_section_by_name('.dynsym')
     for _, sym in enumerate(global_symtab.iter_symbols()):
+      addr = sym.entry['st_value']
       if(sym.entry['st_info']['type'] == 'STT_FUNC'):
-        self.__global_symbols_address.append((sym.entry['st_value'], sym.name))
+        self.__global_symbols_address.append((addr, sym.name))
+        
+        # Ensures that it is getting only functions that have its body inside
+        # '.text' section at the ELF file
+        if(addr >= local_code_begin and addr < local_code_end):
+          self.__local_symbols[sym.name] = []
+          self.__local_symbols_address.append((addr, sym.name))
     
     # This attribute will be used to identify when some funcion is called.
     # We are savind it previouly sorted to use a binary search to query which
     # function is being called via its address.
-    sorted(self.__global_symbols_address)
-    sorted(self.__local_symbols_address)
+    self.__global_symbols_address.sort()
+    self.__local_symbols_address.sort()
   
   def __parse_local_functions(self):
     code = self.__elf.get_section_by_name('.text')
     asm_code = code.data()
     start_header_addr = code['sh_addr']
+    
 
     # The capstone Cs creation can be automated dinamically using the following methods and attributes:
       # self.__elf.get_machine_arch()
@@ -52,8 +70,21 @@ class Objdump:
     
     # With this property switched on, we will be able to obtain more information about each instrucion
     md.detail = True
-    for ins in md.disasm(asm_code, start_header_addr):     
-      print(f'0x{ins.address:x}:\t{ins.mnemonic}\t{ins.op_str}')
+    
+    j = 0
+    for (idx,name) in  self.__local_symbols_address:
+      print(f'{idx:x}', name)
+      j+=1
+      if(j>5): break
+
+    func_index = 0
+    for ins in md.disasm(asm_code, start_header_addr): 
+      # print(f'0x{ins.address:x}:\t{ins.mnemonic}\t{ins.op_str}')
+      if(func_index < len(self.__local_symbols_address)-1 and
+        ins.address >= self.__local_symbols_address[func_index+1][0]):
+        func_index += 1
+      func_name = self.__local_symbols_address[func_index][1]
+      self.__local_symbols[func_name].append(ins)
 
 import sys
 Objdump(sys.argv[1])
